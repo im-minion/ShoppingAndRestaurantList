ef3516fcf40958dd4d67fac2c6059cba8b69ada4
im-minion <vaibhavminiyar@gmail.com>
2017-09-18 20:42:08 +0530

    Recipe List Done understood :)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,8 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('TestName', 'TestDesc', 'http://www.foodtolove.co.nz/assets/images/badge-collection.png'),
+    new Recipe('TestName', 'TestDesc', 'http://www.foodtolove.co.nz/assets/images/badge-collection.png')
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
